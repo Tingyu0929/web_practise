5c8b404c21714a7575cc7facc71f2b5c3a9df585
Stacy <gty20020929@gmail.com>
2025-10-09 00:18:35 +0800

    更改

--- acg-fronted/vite.config.js
+++ acg-fronted/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'acg.404-studio.work',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
